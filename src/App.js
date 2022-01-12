da85bd749c7390517bdbaf209e2eee82df6c0d9f
akariyan <fntlsltm@gmail.com>
2022-01-12 20:18:33 +0900

    chore: serverless 최종 테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Serverless
+          Learn AWS and Serverless
         </a>
       </header>
     </div>
